5d03a170feb14753aca04853cd730fabce684634
Teju <126788015+tejateju371@users.noreply.github.com>
2024-10-12 11:15:50 +0530

    Update Navbar.jsx
    Weavelife website link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -107,8 +107,8 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
       <header>
         <nav className="navbar navbar-expand-lg fixed-top">
           <div className="container-fluid">
-            <a className="navbar-brand" href="https://telusko.com/">
-              Telusko
+            <a className="navbar-brand" href="https://weavelife.site/">
+              WeaveLife 
             </a>
             <button
               className="navbar-toggler"
